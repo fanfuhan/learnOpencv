da718f8596029b066f4cde9fc70ecfc8be122946
fanfuhan <17600982551@163.com>
2019-03-23 19:26:04 +0800

    006

--- 006-查找表(LUT)的使用.cpp
+++ 006-查找表(LUT)的使用.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 using namespace cv;
 
+// 自定义LUT
+Mat &myColorMap(Mat &image);
+
 /*
  *  Look Up Table(LUT)查找表的使用
  */
@@ -15,7 +18,34 @@ int main() {
     Mat dst;
     applyColorMap(src, dst, COLORMAP_SUMMER);
     imshow("colorMap", dst);
+
+    // 使用自己的LUT
+    Mat my_dst, gray;
+    cvtColor(src, gray, COLOR_BGR2GRAY);
+    my_dst = myColorMap(gray);
+    imshow("my_dst", my_dst);
+
     waitKey(0);
     return 0;
+}
+
+// 自定义LUT
+Mat &myColorMap(Mat &image) {
+    int lut[256];
+    for (int i = 0; i < 256; ++i) {
+        if (i < 127)
+            lut[i] = 0;
+        else
+            lut[i] = 255;
+    }
+
+    for (int row = 0; row < image.rows; ++row) {
+        for (int col = 0; col < image.cols; ++col) {
+            int pv = image.at<uchar>(row, col);
+            image.at<uchar>(row, col) = lut[pv];
+        }
+    }
+
+    return image;
 }
 
