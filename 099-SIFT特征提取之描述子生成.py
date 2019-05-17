47e93e4ff1a4d5d3fa60eb885d19a8bd2e815dc0
fanfuhan <17600982551@163.com>
2019-05-17 16:26:19 +0800

    099

---
+++ 099-SIFT特征提取之描述子生成.py
@@ -0,0 +1,26 @@
+"""
+SIFT特征提取 – 描述子生成
+"""
+
+import cv2 as cv
+
+box = cv.imread("D:/images/box.png")
+box_in_sence = cv.imread("D:/images/box_in_scene.png")
+cv.imshow("box", box)
+cv.imshow("box_in_sence", box_in_sence)
+
+# 创建sift特征检测器
+sift = cv.xfeatures2d.SIFT_create()
+kp1, des1 = sift.detectAndCompute(box,None)
+kp2, des2 = sift.detectAndCompute(box_in_sence,None)
+
+# 暴力匹配
+bf = cv.DescriptorMatcher_create(cv.DescriptorMatcher_BRUTEFORCE)
+matches = bf.match(des1,des2)
+
+# 绘制匹配
+matches = sorted(matches, key = lambda x:x.distance)
+result = cv.drawMatches(box, kp1, box_in_sence, kp2, matches[:15], None)
+cv.imshow("orb-match", result)
+cv.waitKey(0)
+cv.destroyAllWindows()
